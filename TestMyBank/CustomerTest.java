1ad1bb17260c426e960f06e762caa94462bac35d
cph-sl444 <cph-sl444@cphbusiness.dk>
2023-02-01 10:36:21 +0100

    signes ændringer part 2

--- TestMyBank/CustomerTest.java
+++ TestMyBank/CustomerTest.java
@@ -1,32 +1,58 @@
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class CustomerTest {
+    Customer testCustomer;
+    String name;
+    Status status;
+    int id;
 
-    @org.junit.jupiter.api.Test
-    void addAccount() {
+    @BeforeEach
+    void setUpCustomer() {
+        new Customer("signe",Status.ADULT,123);
     }
 
-    @org.junit.jupiter.api.Test
+
+
+    @Test
+    void testAddAccount() {
+        assertEquals(name,testCustomer.getName());
+        assertEquals(id,testCustomer.getId());
+        assertEquals(status,testCustomer.getStatus());
+
+    }
+
+    @Test
     void remove() {
+
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setName() {
+        assertEquals(name,testCustomer.setName());
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setStatus() {
+        assertEquals(status,testCustomer.getStatus());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getName() {
+        assertEquals(name,testCustomer.getName());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getStatus() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getId() {
+        assertEquals(id,testCustomer.getId());
     }
 }
